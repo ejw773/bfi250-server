4f2e4329d554d68c31e5d4971627bc45b44ca0ab
ejw773 <ejw773@gmail.com>
2021-01-17 18:22:20 -0500

    final commit for a while...

--- index.js
+++ index.js
@@ -1,17 +1,36 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+
 const jsonParser = bodyParser.json();
+const textParser = bodyParser.text();
 const urlencodedParser = bodyParser.urlencoded({ extended: false });
+
 const Sequelize = require('sequelize');
 const { User } = require('./models');
 const { Status } = require('./models');
 
-
+// Get
 app.get('/', (req, res) => {
   res.send('Hello World!')
 });
 
+app.get('/status', urlencodedParser, async (req, res) => {
+  const status = await Status.findAll();
+  res.json(status);
+});
+
+app.get('/status/:userid', urlencodedParser, async (req, res) => {
+  console.log(req.params);
+  const status = await Status.findAll({
+    where: {
+      userID: req.params.userid
+    }
+  });
+  res.json(status);
+})
+
+// Post
 app.post('/users', urlencodedParser, async (req, res) => {
   console.log(req.body);
   const { name, email } = req.body;
@@ -24,25 +43,7 @@ app.post('/users', urlencodedParser, async (req, res) => {
   })
 });
 
-app.get('/status', urlencodedParser, async (req, res) => {
-  const status = await Status.findAll({
-    include: [{
-      model: Status
-    }]
-  });
-  res.json(status);
-});
-
-app.get('/status/:userid', urlencodedParser, async (req, res) => {
-  const status = await Status.findAll({
-    where: {
-      userID: 1
-    }
-  });
-  res.json(status);
-})
-
-app.post('/status', urlencodedParser, async (req, res) => {
+app.post('/status', jsonParser, async (req, res) => {
   console.log(req.body);
   const { userID, imdbID, status } = req.body;
   const newStatus = await Status.create({
@@ -57,8 +58,23 @@ app.post('/status', urlencodedParser, async (req, res) => {
   })
 });
 
+// Delete
+app.delete('/delete/:userID/:imdbID', async (req, res) => {
+  console.log(req.params);
+  const { userID, imdbID } = req.params;
+  const deleteStatus = await Status.destroy({
+    where: {
+      userID,
+      imdbID
+    }
+  });
+  res.json({
+    userID,
+    imdbID
+  })
+})
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Server listening at http://localhost:${port}`)
 });
